9d442a90b028aa94fbe1b27b9863cb0c33c1bba6
Igor Mestechkin <Igor.mestechkin@gmail.com>
2022-02-20 00:45:07 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution here
+function findMatching(array, name) {
+    let check = array.filter(ele => ele.toLowerCase() === name.toLowerCase())
+    
+    return check
+  }
+
+  function fuzzyMatch(array, letters) {
+    let check = array.filter(ele => ele[0] === letters[0])
+    
+    return check
+  }
+
+
+  function matchName(array, name) {
+    let check = array.filter(ele => ele.name === name)
+      console.log(check)
+      return check
+    }
+
+
+
